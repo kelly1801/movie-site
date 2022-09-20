32887612c7a3a219d132927392aa979f98777845
¨Kelly <fabianachara@gmail.com>
2022-09-20 17:10:47 -0500

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SearchPage from '.movie-site/src/pages/SearchPage.jsx'
+import SearchPage from './movie-site/src/pages/SearchPage.jsx'
 import Home from "./pages/Home";
 
 import Footer from "./components/footer";

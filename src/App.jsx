2e3b408de6908e3c0b333aa4115ebb64907c0ec1
¨Kelly <fabianachara@gmail.com>
2022-09-20 16:58:59 -0500

    finish home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SearchPage from './pages/SearchPage'
-import Home from './pages/Home'
+import Home from "./pages/Home";
 
-import Footer from './components/footer'
-import MovieInfo from './pages/movieInfo'
+import Footer from "./components/footer";
+import MovieInfo from "./pages/movieInfo";
 function App() {
-  
-
   return (
-    
     <BrowserRouter>
-    <div >
-    
-    <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/search'element={<SearchPage/>}></Route>
-        <Route path='/search/:id'element={<MovieInfo />}></Route>
-      </Routes>
-<Footer/>
-      </div> 
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/search" element={<SearchPage />}></Route>
+          <Route path="/search/:id" element={<MovieInfo />}></Route>
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
-  
-  )
+  );
 }
 
-export default App
+export default App;
